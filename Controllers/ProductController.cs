94c8b61403d2b82eeb34da28550fd12070df2c45
rakibul alam <rakibulalam109@gmail.com>
2025-02-13 12:31:38 +0600

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pagination.Models;
+
+namespace Pagination.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private List<Products> products = new List<Products>();
+
+        public ProductController()
+        {
+            // create some mock data
+            for (int i = 1; i <= 100; i++)
+            {
+                products.Add(new Products { Id = i, Name = "Product " + i, Price = i * 2.0 });
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Products> GetProducts(int page , int pageSize)
+        {
+            var totalProducts = products.Count;
+            var totalPages = Math.Ceiling((decimal)totalProducts / pageSize);
+            var productPerPage = products.Skip((page-1)*pageSize).Take(pageSize);
+            return productPerPage;
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,9 @@
+﻿namespace Pagination.Models
+{
+    public class Products
+    {
+        public int Id { get;set; }
+        public string? Name { get;set; }
+        public double Price { get; set; }
+    }
+}
